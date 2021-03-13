ecaee3b87dc91f21e9d969110594c9aa9da1785b
leanagoetze <lrgoetze@ucsb.edu>
2021-03-13 13:31:39 -0800

    testing

--- tests/testthat/test_fish_distribution.R
+++ tests/testthat/test_fish_distribution.R
@@ -1,7 +1,7 @@
 test_that("fish_distribution_works", {
   library(tidyverse)
   fish_data = as.data.frame(cbind(x=c("salmon", "tuna", "shark", "salmon", "salmon", "tuna")))
-  expect_equal(fish_distribution(fish_data=fish_data_new)$"Rarest Fish", "shark")
-  expect_equal(fish_distribution(fish_data=fish_data_new)$"Total Fish", 6)
-  expect_equal(fish_distribution(fish_data=fish_data_new)$"Most Common Fish", "salmon")
+  expect_equal(fish_distribution(fish_data)$"Rarest Fish", "shark")
+  expect_equal(fish_distribution(fish_data)$"Total Fish", 6)
+  expect_equal(fish_distribution(fish_data)$"Most Common Fish", "salmon")
   })
