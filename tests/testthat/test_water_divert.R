bc33681e8c65e50342c0dbb70a19952ec38208e2
alexmilward <amilward@ucsb.edu>
2021-03-13 18:17:19 -0800

    finalized water_divert tests

--- tests/testthat/test_water_divert.R
+++ tests/testthat/test_water_divert.R
@@ -1,5 +1,5 @@
 test_that("water_divert_works", {
   expect_equal(water_divert(diversion = 0), 0)
   expect_equal(water_divert(diversion = 1), 396.6)
-  expect_length(water_divert(diversions = c(1:3)), 3)
+  expect_true((water_divert(diversions = 10)) > (water_divert(diversion = 5)))
 })
